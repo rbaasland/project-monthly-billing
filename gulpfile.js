38fef0e9fc8e6ac3fd4daef15daecf962fa2a526
rbaasland <rbaasland@gerrit.wikimedia.org>
2015-10-24 13:15:06 -0400

    Added bootstrap & font awesome

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ var sass = require('gulp-sass');
 
 // Compile Our Sass
 gulp.task('sass', function() {
-    return gulp.src('css/*.scss')
+    return gulp.src('css/main.scss')
         .pipe(sass())
         .pipe(gulp.dest('css'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('css/*.scss', ['sass']);
+    gulp.watch('css/main.scss', ['sass']);
 });
 
 // Default Task
